b46fe33fe8a6c0dee828cda3bc968bf0303c625c
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2020-09-15 20:41:38 +0100

    setup di for view models and common factory

--- app/src/main/java/com/yb/corethree/di/AppComponent.kt
+++ app/src/main/java/com/yb/corethree/di/AppComponent.kt
@@ -5,7 +5,7 @@ import dagger.Component
 import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [AppModule::class])
+@Component(modules = [AppModule::class, ViewModelModule::class])
 interface AppComponent {
 
     fun inject(activity: MainActivity)
---
+++ app/src/main/java/com/yb/corethree/di/ViewModelModule.kt
@@ -0,0 +1,13 @@
+package com.yb.corethree.di
+
+import androidx.lifecycle.ViewModelProvider
+import dagger.Binds
+import dagger.Module
+
+@Module
+abstract class ViewModelModule {
+
+    @Binds
+    abstract fun bindViewModelFactory(factory: ViewModelFactory): ViewModelProvider.Factory
+
+}
