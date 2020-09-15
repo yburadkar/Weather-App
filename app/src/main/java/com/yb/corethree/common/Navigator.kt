bac193eff6799b03e234d47a18cf045ea0df3acb
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2020-09-15 20:49:37 +0100

    add navigator and toolbar manager for single activity MVVM architecture

---
+++ app/src/main/java/com/yb/corethree/common/Navigator.kt
@@ -0,0 +1,22 @@
+package com.yb.corethree.common
+
+import io.reactivex.subjects.PublishSubject
+
+object Navigator {
+
+    private val navigationSubject = PublishSubject.create<NavigationEvent>()
+
+    fun onNavigationEvent(): io.reactivex.Observable<NavigationEvent> {
+        return navigationSubject
+    }
+
+    fun sendNavigationEvent(event: NavigationEvent) {
+        navigationSubject.onNext(event)
+    }
+
+}
+
+sealed class NavigationEvent
+object WeatherNavigationAccount : NavigationEvent()
+object SearchWeatherNavigationEvent : NavigationEvent()
+object DetailWeatherNavigationEvent : NavigationEvent()
