26e2bad4793ad3faefb4215a8cb2e6d3c17c1f0a
Yogesh Buradkar <yogesh.buradkar@gmail.com>
2020-09-20 14:47:32 +0100

    add navigation test

--- app/src/test/java/com/yb/corethree/features/weather/search/SearchViewModelTest.kt
+++ app/src/test/java/com/yb/corethree/features/weather/search/SearchViewModelTest.kt
@@ -1,24 +1,24 @@
 package com.yb.corethree.features.weather.search
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
-import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.yb.corethree.common.DetailWeatherNavigationEvent
 import com.yb.corethree.common.Navigator
+import com.yb.corethree.common.SimpleIdlingResource
 import com.yb.corethree.common.ToolbarManager
 import com.yb.corethree.domain.entities.CityList
 import com.yb.corethree.domain.repos.ICurrentWeatherRepository
+import com.yb.corethree.models.City
 import io.mockk.mockk
 import io.mockk.spyk
+import io.mockk.verify
 import io.reactivex.schedulers.Schedulers
 import org.hamcrest.CoreMatchers.`is`
 import org.hamcrest.MatcherAssert.assertThat
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
 class SearchViewModelTest {
-
 
     @get:Rule
     var instantExecutorRule = InstantTaskExecutorRule()
@@ -27,14 +27,14 @@ class SearchViewModelTest {
     private val cityList: CityList = mockk()
     private val weatherRepo = mockk<ICurrentWeatherRepository>()
     private val trampoline = Schedulers.trampoline()
-    private val navigator = Navigator // spyk<Navigator>()
+    private val navigator = spyk<Navigator>()
     private val toolbarManager = spyk<ToolbarManager>()
+    private val idlingResource = SimpleIdlingResource
 
     @Before
     fun setUp() {
-        searchViewModel = SearchViewModel(cityList, weatherRepo, trampoline, trampoline, navigator, toolbarManager)
+        searchViewModel = SearchViewModel(cityList, weatherRepo, trampoline, trampoline, navigator, toolbarManager, idlingResource)
     }
-
 
     @Test
     fun updateSearchText_UpdatesPrevSearchTerm_IfCalledFirstTime() {
@@ -52,6 +52,13 @@ class SearchViewModelTest {
         searchViewModel.updateSearchText(newText)
         val result = searchViewModel.prevSearchTerm
         assertThat(result, `is`(newText))
+    }
+
+    @Test
+    fun navigateToDetail() {
+        val city = mockk<City>()
+        searchViewModel.navigateToDetail(city)
+        verify { navigator.sendNavigationEvent(DetailWeatherNavigationEvent(city = city)) }
     }
 
 }
